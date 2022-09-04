9a94f77f9c3cbd074c51e200b97c7ea9f36f454f
Blai234 <44668994+Blai234@users.noreply.github.com>
2022-09-04 20:48:20 +0200

    Update amadeus.py

--- amadeus.py
+++ amadeus.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "Your OpenAI key"
+    openai.api_key = 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
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "Your Telegram Key"
+    tgkey = 4df18b09e3bfb41635ebd0cc7ac451b6
 else:
     tgkey = tgenv
 print(tgenv)
